1545eb0c28e045c995a5a80439aec08ff8e7f9be
Zeriuso <oleglizunov94@mail.ru>
2024-10-20 08:35:46 +0300

    OpenGL init

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,44 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int WINDOW_WIDTH = 550;
+const unsigned int WINDOW_HEIGHT = 800;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter", nullptr, nullptr);
     if (!window)
     {
+        std::cout << "Failed to glfwCreateWindow" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetKeyCallback(window, key_callback);
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -28,10 +47,8 @@ int main(void)
         return -1;
     }
 
-    glClearColor(0, 0, 1, 0.5);
-
-	
-
+    glClearColor(0, 0, 1, 0);
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
